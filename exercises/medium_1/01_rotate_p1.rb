0676d44caec22b71fbce418036dda4bf33ef0ea2
endrjuki <endrju.vilcmeiers@gmail.com>
2020-03-15 12:32:16 +0200

    Added LS solution

--- exercises/medium_1/01_rotate_p1.rb
+++ exercises/medium_1/01_rotate_p1.rb
@@ -45,10 +45,8 @@ def rotate_array(arr)
   new_array.push(new_array.shift)
 end
 
-rotate_array([7, 3, 5, 2, 9, 1]) == [3, 5, 2, 9, 1, 7]
-rotate_array(['a', 'b', 'c']) == ['b', 'c', 'a']
-rotate_array(['a']) == ['a']
+# LS solution (still better...)
 
-x = [1, 2, 3, 4]
-rotate_array(x) == [2, 3, 4, 1]   # => true
-x == [1, 2, 3, 4]         
+def rotate_array(arr)
+  arr[1..-1] + [arr[0]]
+end
