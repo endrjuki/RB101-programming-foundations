dfd357fd23def11cd52a9090669889f42a092093
endrjuki <endrju.vilcmeiers@gmail.com>
2020-03-30 11:05:44 +0300

    revisiting this exercise

--- exercises/easy_1/2_odd.rb
+++ exercises/easy_1/2_odd.rb
@@ -3,10 +3,25 @@
 # This method returns true if the number's absolute value is odd.
 # You may assume that the argument is a valid integer value.
 
+# Input
+# - integer
+
+# Output
+# - boolean
+#   - true if input integer odd
+#   - false otherwise
+
+# How I would do it now
 def integer_odd?(int)
-  int % 2 != 0
+  int.abs.odd?
 end
 
-def integer_odd2?(int)
-  int.remainder(2) != 0
+# my first solution
+def integer_odd?(int)
+  int.abs % 2 != 0
+end
+
+# another solution
+def integer_odd?(int)
+  int.abs.remainder(2) != 0
 end
