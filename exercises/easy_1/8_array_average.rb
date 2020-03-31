571916c97c87a3e73a7df2eacdc742cc9fbe245f
endrjuki <endrju.vilcmeiers@gmail.com>
2020-03-31 17:34:10 +0300

    cleanup

--- exercises/easy_1/8_array_average.rb
+++ exercises/easy_1/8_array_average.rb
@@ -2,43 +2,18 @@
 # and returns the average of all numbers in the array.
 # The array will never be empty and the numbers will always be positive integers.
 
-#Input:
-#  array
-#    -contains integers
-#    -will never be empty
-#    -always contains positive integers
+# Input
+# - array
+#   - contains integers
 
-#Output:
-#  integer or string
-#    -average of all numbers in the array
+# Output
+# - integer
+#   - average of of all numbers in the array
 
-#Algorithm:
-#  -define a method `average` that takes one argument: `arr`
-#  -initialize variable `sum` with value `0`
-#  -iterate through array, sum each element with variable `sum`
-#  -divie sum by the size of the array and return the value
+# Algorithm
+# - sum all numbers in the array
+# - divide the sum by the number of elements in the array
 
 def average(arr)
-  sum = 0
-  arr.each {|element| sum += element}
-  sum / arr.size
+  arr.reduce(:+) / arr.length.to_f
 end
-
-#ls way
-def average(arr)
-  sum = arr.reduce {|sum, number| sum + number}
-  sum / arr.size
-end
-# also can use symbol like so
-def average(arr)
-  sum = arr.reduce(:+)
-  sum / arr.size
-end
-
-# Further exploration: dealing with floats
-def average(arr)
-  sum = arr.reduce(:+)
-  sum / arr.size.to_f
-end
-
-
