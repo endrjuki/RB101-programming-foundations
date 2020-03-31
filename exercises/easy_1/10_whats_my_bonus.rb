79fef002efac1b0e59d1fcc9140cfa52d2a60c04
endrjuki <endrju.vilcmeiers@gmail.com>
2020-03-31 17:49:16 +0300

    cleaning up nonsense

--- exercises/easy_1/10_whats_my_bonus.rb
+++ exercises/easy_1/10_whats_my_bonus.rb
@@ -5,41 +5,15 @@
 # If the boolean is false, the bonus should be 0.
 
 # Input
-#   integer:
-#     -positive
-#   boolean:
-#     -true/false
-#
-# Is input validation necessary?
-# Do we need to handle floats?
-#
-# Algorithm
-#   -if input boolean is `false`#
-#     -program returns 0
-#   -if boolean `true`
-#     -return input integer divided by two
+# - integer
+#   - represents salary
+# - boolean
+#   - if true -> entitled to bonus, which is half of the salary
 
-def calculate_bonus(int, boolean)
-  case boolean
-  when false
-    return 0
-  when true
-    int / 2
-  end
-end
+# Output
+# - integer
+#   - represents bonus amount
 
-# another way
-def calculate_bonus(int, boolean)
-  return 0 if boolean == false
-  int / 2
-end
-
-# ls way with ternary operator
 def calculate_bonus(salary, bonus)
-  bonus ? (salary / 2) : 0
+  bonus ? salary / 2.0 : 0
 end
-
-# Examples/Test Cases
-puts calculate_bonus(2800, true) == 1400
-puts calculate_bonus(1000, false) == 0
-puts calculate_bonus(50000, true) == 25000
